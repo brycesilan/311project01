7ce7486cff49357c919a164ce34f4a866d62d22e
Connor <38024715+connorric@users.noreply.github.com>
2018-08-29 12:26:56 -0700

    idk

--- party.cpp
+++ party.cpp
@@ -5,3 +5,17 @@
 #include "party.h"
 
 using namespace std;
+
+//class constructor definition
+Party::Party() {
+  /*might not be needed
+  reservationName=NULL;
+  numDiners=0;
+  timeRequired=0;
+  */
+}
+
+//class destructor definition
+Party::~Party() {
+  //TODO free up stuff
+}
